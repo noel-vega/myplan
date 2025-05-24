235e0cf88ce7c04b1b514f98bf41ec4e8a9cb5b9
noel-vega <noelvegajr94@gmail.com>
2025-05-23 21:40:44 -0400

    date handling

--- src/app/newborn-feeding/page.tsx
+++ src/app/newborn-feeding/page.tsx
@@ -12,7 +12,7 @@ export default async function Page() {
       <section className="flex-1">
         <div className="flex items-end justify-between text-2xl font-semibold mb-4">
           <p className="text-3xl">Feeding</p>
-          <p>{format(date.toUTCString(), "MM-dd-yyyy")}</p>
+          <p>{format(date.toLocaleDateString(), "MM-dd-yyyy")}</p>
         </div>
         <FeedingsTable data={feedings} />
       </section>
